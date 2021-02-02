9b29a0a106bb7cd0f4cdd2bcda196aeb3c736da7
fuyao.bill <fuyao.bill@bytedance.com>
2021-02-02 12:03:35 +0800

    210202

---
+++ src/newquestion/LC424.java
@@ -0,0 +1,26 @@
+package newquestion;
+
+public class LC424 {
+    public int characterReplacement(String s, int k) {
+        int[] nums = new int[26];
+        int len = s.length();
+        char[] arr = s.toCharArray();
+        int l = 0, r = 0, max = 0;
+        while(r < len) {
+            int cur = arr[r] - 'A';
+            nums[cur]++;
+            max = Math.max(max, nums[cur]);
+            if(r - l + 1 - max > k) {
+                nums[arr[l] - 'A']--;
+                l++;
+            }
+            r++;
+        }
+        return r - l;
+    }
+
+    public static void main(String[] args) {
+        int i = new LC424().characterReplacement("AAAA", 2);
+        System.out.println(i);
+    }
+}
