1cfa874c7fea98c35ad46a82915be7e5e1f3a45a
fuyao.bill <fuyao.bill@bytedance.com>
2021-03-03 11:44:34 +0800

    210303m

--- src/LCNQ/LC338.java
+++ src/LCNQ/LC338.java
@@ -3,9 +3,7 @@ package LCNQ;
 public class LC338 {
     public int[] countBits(int num) {
         int[] res = new int[num + 1];
-        for(int i = 0;i <= num;i++) {
-            res[i] = Integer.bitCount(i);
-        }
+        for(int i = 0;i <= num;i++) res[i] = res[i & (i - 1)] + 1;
         return res;
     }
 }
