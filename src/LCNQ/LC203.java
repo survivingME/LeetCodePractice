9703242682282bd653a0fcb63baea644e12ca6be
fuyao.bill <fuyao.bill@bytedance.com>
2021-06-05 15:50:03 +0800

    210605

---
+++ src/LCNQ/LC203.java
@@ -0,0 +1,32 @@
+package LCNQ;
+
+import domain.ListNode;
+
+public class LC203 {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        /**
+         * prev指针保存命中元素之前的元素
+         */
+        ListNode prev = dummy, cur = head;
+        while(cur != null) {
+            if(cur.val == val) {
+                prev.next = cur.next;
+                cur = cur.next;
+            } else {
+                prev = cur;
+                cur = cur.next;
+            }
+        }
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(7);
+        head.next = new ListNode(7);
+        head.next.next = new ListNode(7);
+
+        ListNode res = new LC203().removeElements(head, 7);
+    }
+}
