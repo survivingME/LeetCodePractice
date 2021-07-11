dcbcccf8417d1d440ffc9b1fce5d064de2c01cd0
fuyao.bill <fuyao.bill@bytedance.com>
2021-07-11 12:07:47 +0800

    210711

--- src/LCNQ/LC274.java
+++ src/LCNQ/LC274.java
@@ -6,7 +6,7 @@ public class LC274 {
      * 大于len的引用数放在count[len]中
      * 从高向低遍历，记录引用数sum，与可能的h指数作比较，如果大于等于，满足条件返回
      * @param citations
-     * @return
+     * @return h
      */
     public int hIndex(int[] citations) {
         int len = citations.length;
