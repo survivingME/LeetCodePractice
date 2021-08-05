dda1760cffe8bf7eb7d7a993d3a7aefe19e2e916
fuyao.bill <fuyao.bill@bytedance.com>
2021-08-05 20:58:31 +0800

    210805

---
+++ src/LCNQ/LC102.java
@@ -0,0 +1,36 @@
+package LCNQ;
+
+import domain.TreeNode;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class LC102 {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<List<Integer>> res = new LinkedList<>();
+        if(root == null) return res;
+        List<Integer> cur = new LinkedList<>();
+        queue.add(root);
+        cur.add(root.val);
+        res.add(new LinkedList<>(cur));
+        while(!queue.isEmpty()) {
+            cur = new LinkedList<>();
+            int size = queue.size();
+            for(int i = 0;i < size;i++) {
+                TreeNode poll = queue.poll();
+                if(poll.left != null) {
+                    queue.add(poll.left);
+                    cur.add(poll.left.val);
+                }
+                if(poll.right != null) {
+                    queue.add(poll.right);
+                    cur.add(poll.right.val);
+                }
+            }
+            if(cur.size() > 0) res.add(new LinkedList<>(cur));
+        }
+        return res;
+    }
+}
---
+++ src/LCNQ/LC103.java
@@ -0,0 +1,44 @@
+package LCNQ;
+
+import domain.TreeNode;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class LC103 {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<List<Integer>> res = new LinkedList<>();
+        if(root == null) return res;
+        List<Integer> cur = new LinkedList<>();
+        queue.add(root);
+        cur.add(root.val);
+        res.add(new LinkedList<>(cur));
+        boolean right = true;
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            cur = new LinkedList<>();
+            for(int i = 0;i < size;i++) {
+                TreeNode poll = queue.poll();
+                if(poll.left != null) {
+                    queue.add(poll.left);
+                    cur.add(poll.left.val);
+                }
+                if(poll.right != null) {
+                    queue.add(poll.right);
+                    cur.add(poll.right.val);
+                }
+            }
+            if(cur.size() > 0) {
+                if(right) {
+                    Collections.reverse(cur);
+                }
+                res.add(new LinkedList<>(cur));
+                right = !right;
+            }
+        }
+        return res;
+    }
+}
