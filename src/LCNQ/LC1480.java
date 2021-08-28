80a0726d3fef7f0bc25dfc51dc226f8ececf48a4
survivingME <15510385990@163.com>
2021-08-28 19:22:01 +0800

    210828

---
+++ src/LCNQ/LC1480.java
@@ -0,0 +1,10 @@
+package LCNQ;
+
+public class LC1480 {
+    public int[] runningSum(int[] nums) {
+        int[] res = new int[nums.length];
+        res[0] = nums[0];
+        for(int i = 1;i < nums.length;i++) res[i] = res[i - 1] + nums[i];
+        return res;
+    }
+}
